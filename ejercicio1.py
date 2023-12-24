6c19de57ab0deadc4dc5cb1f45ad547bdab7d9d5
Iruneo <3neorueta@gmail.com>
2023-12-24 07:57:20 +0100

    create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,24 @@
+""" 
+1.1 Ejercicio: Crea una variable llamada nombre y asígnale tu nombre como valor.
+Luego, imprime la variable.
+2.1 Ejercicio: Crea dos variables, a y b , y asígnales los valores 5 y 10
+respectivamente. Luego, imprime la suma de a y b .
+3.1 Ejercicio: Calcula el área de un triángulo con base 10 y altura 5.
+4.1 Ejercicio: Calcula el resto de dividir 17 entre 3."""
+
+nombre = "Irune"
+print (nombre)
+
+a = 5
+b = 10
+print (a+b)
+
+print (b*a/2)
+
+a = 17
+b = 3
+print(a/b)
+
+
+
+
