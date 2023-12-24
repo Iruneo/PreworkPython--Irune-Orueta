2dfddb9b85edb531134528e4a82431142d9bab67
Iruneo <3neorueta@gmail.com>
2023-12-24 08:35:56 +0100

    create ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,31 @@
+"""
+1. Ejercicio: Dado un número, imprime si es positivo o negativo. 
+2. Ejercicio: Dado un número, imprime si es par o impar. 
+3. Ejercicio: Dado tres números, encuentra y muestra el mayor de ellos. 
+"""
+
+x = 7
+if x > 0:
+  print ("x es positivo")
+else:
+  print("x no es positivo")
+
+
+x = 12
+if x := 12:
+  print ("este numero es par")
+  
+a = 3
+b = 7 
+c = 12
+
+if a > b and a > c:
+    maximo = a
+    print(maximo = a)
+if b > a and b > c:
+   maximo = b
+   print(maximo = b)
+if c > a and c > b:
+   maximo = c
+   print(maximo)
+
